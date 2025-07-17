48affb96e5767bd5ebc6b0d5ef88a995a8fdd60c
Vinicius E Beuther <vinicius.beuther15@gmail.com>
2025-07-16 23:51:29 -0300

    env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_BRAPI_API_KEY: string;
+    readonly VITE_BRAPI_API_CONSULT_ROUTE: string;
+    readonly VITE_CACHE_KEY: string;
+    readonly VITE_CACHE_TIMESTAMP_KEY: string;
+    readonly VITE_CACHE_DURATION: number; // in milliseconds
+}
