c69edffbe1562b822b0c87b937e0934dfb1b9f7a
Vinicius E Beuther <vinicius.beuther15@gmail.com>
2025-07-16 23:50:06 -0300

    Updated to show the tables in an accordion instead of loading the entire table

--- src/components/InvestmentTable/InvestmentTable.tsx
+++ src/components/InvestmentTable/InvestmentTable.tsx
@@ -1,76 +1,93 @@
 import React, { useState } from 'react';
-import { Data } from '../../types/data.ts';
+import { BRAPIResponse, Data } from '../../types/data.ts';
+import { formatAmount, getDataFromLocalStorage } from '../../utils/utils.ts';
 import {
-  Select,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-  SelectLabel,
-  SelectGroup,
-  SelectContent
-} from '../ui/select.tsx';
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent
+} from '../ui/accordion';
+import { MarketUtils } from '../../utils/marketUtils.ts';
 
 interface InvestmentTablePropsInterface {
   assets: Data[];
 }
 
 const InvestmentTable: React.FC<InvestmentTablePropsInterface> = ({ assets }) => {
-  const [tableFilter, setTableFilter] = useState<string>('all');
+  const MarketClass = new MarketUtils();
+  const marketData: BRAPIResponse[] = getDataFromLocalStorage() || [];
+
+  // Group assets by category
+  const grouped = assets.reduce<Record<string, Data[]>>((acc, asset) => {
+    const key = asset.Tipo;
+    if (!acc[key]) acc[key] = [];
+    acc[key].push(asset);
+    return acc;
+  }, {});
 
   return (
-    <section className='p-5 rounded-[24px]'>
-      <article className='flex items-center justify-end'>
-        <Select onValueChange={(value) => setTableFilter(value)}>
-          <SelectTrigger className='bg-transparent w-[25%] outline-none focus:outline-none rounded-full'>
-            <SelectValue placeholder='Categoria' />
-          </SelectTrigger>
-          <SelectContent className="focus:outline-none outline-none ring-0">
-            <SelectGroup>
-              <SelectLabel>Ativos</SelectLabel>
-              <SelectItem value='all'>Todos</SelectItem>
-              <SelectItem value='Ação'>Ações</SelectItem>
-              <SelectItem value='fii'>FIIs</SelectItem>
-              <SelectItem value='fiagro'>FIAGRO</SelectItem>
-              <SelectItem value='tesouro direto'>Tesouro</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </article>
+    <section className='p-6 rounded-[24px] bg-white shadow-md overfllow-x-auto'>
+      <h2 className='text-xl font-semibold mb-4 text-gray-800'>Carteira de Investimentos</h2>
 
-      <article className='rounded-5xl py-2 text-sm mt-4'>
-        <table className='w-full rounded-lg shadow-lg border-[1px] border-gray-200'>
-          <thead>
-            <tr className='text-center bg-secondary-500'>
-              <th className='w-[1/5] p-2 rounded-l-md'>Código do Ativo</th>
-              <th className='w-[1/5] p-2'>Quantidade</th>
-              <th className='w-[1/5] p-2'>Preço Médio</th>
-              <th className='w-[1/5] p-2'>Posição</th>
-              <th className='w-[1/5] p-2 rounded-r-md'>Categoria</th>
-            </tr>
-          </thead>
-          <tbody className='text-center'>
-            {assets
-              .sort((a, b) => b.Quantidade - a.Quantidade)
-              .filter((asset) =>
-                tableFilter.toLowerCase() === 'all'
-              
-              ? true
-              : asset.Tipo.toLowerCase() === tableFilter.toLowerCase()
-            )
-            .map((asset, idx) => (
-                <tr key={idx} className='text-gray-800 border-b-[1px]'>
-                  <td className='p-1 w-[1/5]'>{asset['Código de Negociação']}</td>
-                  <td className='p-1 w-[1/5]'>{asset.Quantidade}</td>
-                  <td className='p-1 w-[1/5]'>{asset['Preço Médio'].toLocaleString("pt-BR", {currency: "BRL", style: "currency"})}</td>
-                  <td className='p-1 w-[1/5]'>{asset.Valor.toLocaleString("pt-BR", {currency: "BRL", style: "currency"})}</td>
-                  <td className='p-1 w-[1/5]'>{asset.Tipo}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </article>
+      <Accordion type="multiple" className="w-full space-y-2">
+        {Object.entries(grouped)
+        .sort(([a], [b]) => a.localeCompare(b))
+        .map(([category, items]) => (
+          <AccordionItem key={category} value={category} className="border rounded-lg">
+            <AccordionTrigger 
+              className="px-4 py-2 text-left font-medium text-gray-800 bg-gray-200 border-gray-300 hover:bg-gray-300"
+            >
+              <span className='w-full'>
+                {category} 
+                <p className='mt-0'>Quantidade: {items.length}</p>
+              </span>
+            </AccordionTrigger>
+            <AccordionContent className="space-y-3 px-4 py-2">
+              {items
+                .sort((a, b) => b.Quantidade - a.Quantidade)
+                .map((asset, idx) => (
+                  <div
+                    key={idx}
+                    className="flex items-center gap-4 bg-white border rounded-lg p-3 shadow-sm"
+                  >
+                    <img
+                      src={MarketClass.getLogoForAsset(asset, marketData)}
+                      alt={`logo-${asset['Código de Negociação']}`}
+                      className="w-10 h-10 rounded-md object-contain"
+                    />
+                    <div className="flex-1 grid grid-cols-2 md:grid-cols-4 gap-y-1 text-sm">
+                      <div>
+                        <span className="text-gray-500"></span> {asset['Código de Negociação']}
+                      </div>
+                      <div>
+                        <span className="text-gray-500">Quantidade:</span> {asset.Quantidade}
+                      </div>
+                      <div>
+                        <span className="text-gray-500">Preço Atual:</span>{' '}
+                        {MarketClass.getPriceBySymbol(asset['Código de Negociação']) !== undefined
+                          ? formatAmount(MarketClass.getPriceBySymbol(asset['Código de Negociação']))
+                          : '--'}
+                      </div>
+                      <div>
+                        <span className="text-gray-500">Preço Médio:</span>{' '}
+                        {formatAmount(asset['Preço Médio'])}
+                      </div>
+                      <div>
+                        <span className="text-gray-500">Valor:</span>{' '}
+                        {asset.Valor.toLocaleString('pt-BR', {
+                          style: 'currency',
+                          currency: 'BRL'
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </section>
   );
 };
-//https://img.logo.dev/google.com?token=pk_C0qwQwS1QFOVP-DBvizQrg
+
 export default InvestmentTable;
