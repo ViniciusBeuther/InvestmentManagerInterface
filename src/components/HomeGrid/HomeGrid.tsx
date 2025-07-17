efe0bcbfd636999a98e18854c6f49637cf4e5f79
Vinicius E Beuther <vinicius.beuther15@gmail.com>
2025-07-16 23:49:21 -0300

    Updated home page layout

--- src/components/HomeGrid/HomeGrid.tsx
+++ src/components/HomeGrid/HomeGrid.tsx
@@ -2,21 +2,27 @@ import { Data } from '../../types/data';
 import InvestmentTable from '../InvestmentTable/InvestmentTable';
 import Sidebar from '../Sidebar/Sidebar';
 
-interface HomeGridProps{
+interface HomeGridProps {
   assets: Data[];
 }
 
-const HomeGrid = ( { assets }:HomeGridProps  ) => {
+const HomeGrid = ({ assets }: HomeGridProps) => {
   return (
-    <section className="h-full flex">
-      <article className="w-[20%]">
+    <section className="h-screen flex overflow-hidden">
+      <aside className="w-[20%] overflow-y-auto border-r">
         <Sidebar />
-      </article>
-      <article className='bg-tertiary w-[80%] p-5'>
-        <h1>Carteira de Investimentos</h1>
-        <h2 className='text-gray-600'>Acompanhe os ativos em seu Portfolio.</h2>
-        <InvestmentTable assets={assets} />
-      </article>
+      </aside>
+
+      <main className="w-[80%] p-5 bg-tertiary flex flex-col">
+        <header className="mb-4">
+          <h1 className="text-xl font-bold">Carteira de Investimentos</h1>
+          <h2 className="text-gray-600">Acompanhe os ativos em seu Portfolio.</h2>
+        </header>
+
+        <div className="overflow-y-auto overflow-x-auto max-h-full border rounded-lg">
+          <InvestmentTable assets={assets} />
+        </div>
+      </main>
     </section>
   );
 };
