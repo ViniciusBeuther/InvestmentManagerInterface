61e942e660ae3436cca01bf7ed31e422b93f10f3
Vinicius E Beuther <vinicius.beuther15@gmail.com>
2025-08-01 23:54:07 -0300

    New class for handling portfolio manipulations

---
+++ src/utils/PortfolioUtils.ts
@@ -0,0 +1,52 @@
+import { Data } from "../types/data";
+
+class PortfolioUtils {
+    private data : Data[] = [];
+    private readonly symbolsStringList : string[] = [];
+
+    // constructor to initialize the class with async start method
+    public start = async () => {
+        await this.initialize();
+        await this.setStringListOfSymbols();
+    }
+
+    protected initialize = async () => {
+        try{
+            const response = await fetch( import.meta.env.VITE_API_URL_WALLET );
+            const result = await response.json();
+            this.data = result;
+            console.log( result );
+        } catch ( error ) {
+            console.error("Error initializing PortfolioUtils", error);
+        }
+    };
+
+    
+    public getData = () : Data[] => {
+        return this.data;
+    }
+
+    public getStringListOfSymbols = () : string[] => {
+        return this.symbolsStringList;
+    }
+
+    public getItemInfoBySymbol = ( symbol: string ) : Data | null => {
+        this.data.forEach( ( item: Data ) => {
+            if( symbol.includes( item["Código de Negociação"] ) )
+                return item;
+        } );
+        
+        return null;
+    }
+
+    private readonly setStringListOfSymbols = () => {
+        if (this.data.length == 0) return;
+
+        this.data.forEach(( item:Data ) => this.symbolsStringList.push( item["Código de Negociação"] ) );
+        //console.log("PortfolioUtils.getStringListOfSymbols: ", this.getStringListOfSymbols());
+    }
+
+
+};
+
+export default PortfolioUtils;
